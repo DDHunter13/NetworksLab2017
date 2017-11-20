893a615693f5c6e5337a6f987fb022d24fea6de0
Besedin Denis <nero1310@rambler.ru>
2017-11-20 20:22:33 +0300

    inverse strncmp, fix end push err

--- tcp_template/ftp_tcp_client/main.c
+++ tcp_template/ftp_tcp_client/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
@@ -24,20 +25,21 @@ int addrPaste (char * message) {
 }
 
 int ls (int sockfd, char * arg) {
-    addrPaste(arg);
-    char * post[256];
-    char * mes[256];
+    //addrPaste(arg);
+    char post[256];
+    char mes[256];
     memset(post, 0, 256);
     memset(mes, 0, 256);
     strcat(post, "ls ");
+    strcat(post, addr);
     strcat(post, arg);
     send(sockfd, "256", 3, 0);
     send(sockfd, post, 256, 0);
     readn(sockfd, mes, 1);
-    if (strncmp(mes, "y", 1)) {
+    if (!(strncmp(mes, "y", 1))) {
         memset(mes, 0, 256);
         readn(sockfd, mes, 256);
-        while (!(strncmp(mes, "_ls_end", 7))) {
+        while (strncmp(mes, "_ls_end", 7)) {
             printf(mes);
             printf("\n");
             memset(mes, 0, 256);
@@ -54,21 +56,22 @@ int pull (int sockfd, char * arg) {
         printf("Can't create file \n");
         return 0;
     }
-    addrPaste(arg);
-    char * post[256];
-    char * mes[256];
+    //addrPaste(arg);
+    char post[256];
+    char mes[256];
     memset(post, 0, 256);
     memset(mes, 0, 256);
     strcat(post, "pull ");
+    strcat(post, addr);
     strcat(post, arg);
     send(sockfd, "256", 3, 0);
     send(sockfd, post, 256, 0);
     readn(sockfd, mes, 1);
-    if (strncmp(mes, "y", 1)) {
+    if (!(strncmp(mes, "y", 1))) {
         memset(mes, 0, 256);
         readn(sockfd, mes, 256);
-        while (!(strncmp(mes, "_end_of_file", 256))) {
-            if (strncmp(mes, "/_end_of_file", 256)) {
+        while (strncmp(mes, "_end_of_file", 256)) {
+            if (!(strncmp(mes, "/_end_of_file", 256))) {
                 int i;
                 for (i = 0; i < strlen(mes) - 1; i++)
                     mes[i] = mes[i + 1];
@@ -76,8 +79,10 @@ int pull (int sockfd, char * arg) {
             fwrite((void *) mes, sizeof(char), 256, fp);
             readn(sockfd, mes, 256);
         }
+        fclose(fp);
         return 1;
     }
+    fclose(fp);
     return 0;
 }
 
@@ -87,50 +92,69 @@ int push (int sockfd, char * arg) {
         printf("Can't read file \n");
         return 0;
     }
-    addrPaste(arg);
-    char * post[256];
-    char * mes[256];
+    //addrPaste(arg);
+    char post[256];
+    char mes[256];
     memset(post, 0, 256);
     memset(mes, 0, 256);
     strcat(post, "push ");
+    strcat(post, addr);
     strcat(post, arg);
     send(sockfd, "256", 3, 0);
     send(sockfd, post, 256, 0);
     readn(sockfd, mes, 1);
     int size = 0;
-    if (strncmp(mes, "y", 1)) {
+    if (!(strncmp(mes, "y", 1))) {
         memset(mes, 0, 256);
         fseek(fp, 0, SEEK_SET);
         while (!feof(fp)) {
             size = (int)fread((void *) mes, sizeof(char), 256, fp);
-            if (strncmp(mes, "_end_of_file", 256)) {
+            if (!(strncmp(mes, "_end_of_file", 256))) {
                 int i;
                 for (i = strlen(mes)-1; i >= 0; i--)
                     mes[i + 1] = mes[i];
                 mes[0] = '/';
             }
-            send(sockfd, mes, size, 0);
+            send(sockfd, mes, 256, 0);
         }
+        send(sockfd, "_end_of_file", 256, 0);
+        fclose(fp);
         return 1;
     }
+    fclose(fp);
     return 0;
 }
 
 int addrChange(int sockfd, char * arg){
-    addrPaste(arg);
-    char * post[256];
-    char * mes[256];
+    //addrPaste(arg);
+    char post[256];
+    char mes[256];
     memset(post, 0, 256);
     memset(mes, 0, 256);
     strcat(post, "cd ");
+    strcat(post, addr);
     strcat(post, arg);
     send(sockfd, "256", 3, 0);
     send(sockfd, post, 256, 0);
-    readn(sockfd, mes, 256);
-    if (strncmp(mes, "y", 1)) {
-        memset(addr, 0, 256);
-        strcat(addr, arg);
-        strcat(addr, "/");
+    readn(sockfd, mes, 1);
+    if (!(strncmp(mes, "y", 1))) {
+        //memset(addr, 0, 256);
+        if (!(strncmp(arg, "..", 2))) {
+            int i, f = 0;
+            for(i = 255; i > 0; i--) {
+                if (addr[i] == '/') {
+                    f++;
+                    if (f == 2) {
+                        break;
+                    }
+                }
+                addr[i] = '\0';
+            }
+            addr[0] = '/';
+        } else {
+            strcat(addr, arg);
+            strcat(addr, "/");
+        }
         return 0;
     }
     return 1;
@@ -139,8 +163,15 @@ int addrChange(int sockfd, char * arg){
 int readAndWriteCycle (int sockfd) {
     char buffer[256];
     while (1) {
+        int i;
         memset(buffer, 0, 256);
         fgets(buffer, 255, stdin);
+        for (i = 0; i < 256; i ++) {
+            if (buffer[i] == '\n') {
+                buffer[i] = '\0';
+                break;
+            }
+        }
         int n = parse(sockfd, buffer);
         if (n == 1) break;
         if (n == 2) printf("Unknown command\n");
@@ -162,14 +193,14 @@ int parse(int sockfd, char * message) {
     // сохраним аргументы из оставшейся части строки
     for (; i < strlen(message); i++) arg[j++] = message[i];
 
-    if (strncmp(command, "exit", 4)) {
+    if (!(strncmp(command, "exit", 4))) {
         send(sockfd, "004", 3, 0);
         send(sockfd, command, 4, 0);
         return 1;
-    } else if(strncmp(command, "cd", 2)) addrChange (sockfd, arg);
-        else if(strncmp(command, "ls", 2)) ls (sockfd, arg);
-            else if(strncmp(command, "pull", 4)) pull (sockfd, arg);
-                else if(strncmp(command, "push", 4)) push (sockfd, arg);
+    } else if(!(strncmp(command, "cd", 2))) addrChange (sockfd, arg);
+        else if(!(strncmp(command, "ls", 2))) ls (sockfd, arg);
+            else if(!(strncmp(command, "pull", 4))) pull (sockfd, arg);
+                else if(!(strncmp(command, "push", 4))) push (sockfd, arg);
                     else return 2;
     return 0;
 }
@@ -208,14 +239,14 @@ int main(int argc, char *argv[]) {
     char buffer[256];
     char *p = buffer;
 
-    if (argc < 3) {
-        fprintf(stderr, "usage %s hostname port\n", argv[0]);
-        exit(0);
-    }
+    //if (argc < 3) {
+    //    fprintf(stderr, "usage %s hostname port\n", argv[0]);
+    //    exit(0);
+    //}
 
-    portno = (uint16_t) atoi(argv[2]);
-
-    /* Create a socket point */
+    //portno = (uint16_t) atoi(argv[2]);
+    portno = (uint16_t) 5001;
+            /* Create a socket point */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0) {
@@ -223,7 +254,8 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    server = gethostbyname(argv[1]);
+    //server = gethostbyname(argv[1]);
+    server = gethostbyname("192.168.56.101");
 
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host\n");
